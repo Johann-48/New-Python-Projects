9cc0d2d9d052c7c7eb84aa473890255469911cbe
Johann-48 <135051704+Johann-48@users.noreply.github.com>
2025-05-07 20:08:20 -0300

    Delete button added
    Unfinished, still has bugs

--- To-Do App/main.py
+++ To-Do App/main.py
@@ -1,6 +1,6 @@
 
 # List of Items
-List = []
+items = []
 
 # Model of data Creation
 class Item:
@@ -9,15 +9,19 @@ class Item:
         self.finished = False
 
 # Creates and inserts samples
-n1 = Item('English Homework')
-n2 = Item('Tidy the Room')
-n3 = Item('Learn Pyhton')
-List.extend([n1, n2, n3])
 
+# FUNCTIONS
 # Add Item Function
 def addItem(title):
     new_item = Item(str(title))
-    List.append(new_item)
+    items.append(new_item)
+
+#Add Delete Item Function
+def deleteItem(title):
+    for i in items:
+        if i.title == title:
+            items.remove(i)
+            break
 
 while True:
     title = input('Insert an item: ')
@@ -25,11 +29,25 @@ while True:
 
     while True:
         # Asks the user if it wants to stop
-        continue_input = input('Do you want to add another item? (y/n): ').strip().lower()
+        continue_input = input('Do you want to add another item? (y/n/Show List/Delete): ').strip().lower()
         if continue_input == 'n':
             break
         elif continue_input == 'y':
             break
+        elif continue_input == 'show list':
+            for i in items:
+                print(f'- {i.title}')
+        elif continue_input == 'delete':
+                while True:
+                    itemTitle = str(input('Insert the Item you want to delete: '))
+                    for i in items:
+                        if i.title == itemTitle:
+                            items.remove(i)
+                            print('Item Deleted')
+                            break
+                    else:
+                            print('Item was not found, sure you wrote that right?')
+
         else:
             print('Awnser Unknown')
         
@@ -37,5 +55,5 @@ while True:
         break
 
 # Prints the To-Do List
-for item in List:
-    print(f'- {item.title}')
+for i in items:
+    print(f'- {i.title}')
